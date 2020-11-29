68f81a412d8acccd3413cf9317bd8c49fefc9731
etztrefis <etztrefishello@gmail.com>
2020-11-29 12:08:26 +0700

    draw line from hunter to speedrunner in they are in the end or nether

--- src/me/trefis/speedrunnervsviewers/Worker.java
+++ src/me/trefis/speedrunnervsviewers/Worker.java
@@ -1,8 +1,10 @@
 package me.trefis.speedrunnervsviewers;
 
 import me.trefis.speedrunnervsviewers.context.Roles;
-import org.bukkit.Location;
+import org.apache.commons.lang.Validate;
+import org.bukkit.*;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.PlayerInventory;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.util.Vector;
 
@@ -34,7 +36,25 @@ public class Worker implements  Runnable {
 
         }else {
             player.setCompassTarget(nearest.getLocation());
+            PlayerInventory inventory = player.getInventory();
+            if (inventory.getItemInMainHand().getType() == Material.COMPASS || inventory.getItemInOffHand().getType() == Material.COMPASS) {
+                if (player.getWorld().getEnvironment() == World.Environment.NETHER || nearest.getWorld().getEnvironment() == World.Environment.NETHER
+                        || player.getWorld().getEnvironment() == World.Environment.THE_END || nearest.getWorld().getEnvironment() == World.Environment.THE_END) {
+                    drawDirection(player.getLocation(), nearest.getLocation(), 3);
+                }
+            }
         }
+    }
+
+    private void drawDirection(Location point1, Location point2, double space) {
+        World world = point1.getWorld();
+        Validate.isTrue(point2.getWorld().equals(world), "You have to be in same worlds!");
+        Vector p1 = point1.toVector();
+        Vector dir = point2.toVector().clone().subtract(p1).setY(0).normalize().multiply(space);
+        Vector p = p1.add(dir);
+        Particle.DustOptions dust = new Particle.DustOptions(Color.fromRGB(255, 255, 0), 1);
+        world.spawnParticle(Particle.REDSTONE, p.getX(), p1.getY() + 1.25, p.getZ(), 0, 0, 0, 0, dust);
+
     }
 
     private Player getNearestSpeedrunner(Player player) {
@@ -44,8 +64,8 @@ public class Worker implements  Runnable {
                 .filter(p -> !p.equals(player))
                 .filter(p -> playerData.getRole(p) == Roles.SPEEDRUNNER)
                 .filter(p -> p.getWorld().equals(player.getWorld()))
-                .filter(p -> p.getWorld().getEnvironment().equals(player.getWorld().getEnvironment()))
                 .min(Comparator.comparing(p -> p.getLocation().distance(playerLocation)))
                 .orElse(null);
     }
+
 }
